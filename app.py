043f95ab8b4b126bb11976accf6f6bc694afb727
bc-dipesh <bcdipeshwork@gmail.com>
2023-05-20 11:59:31 -0400

    Implement add new user functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from models import db, connect_db, User
 from dotenv import load_dotenv
 import os
@@ -28,3 +28,29 @@ def home_page():
     users = User.query.all()
 
     return render_template("home.html", users=users)
+
+
+@app.route("/users/create")
+def create_user_form():
+    """Display create user form page"""
+
+    return render_template("create_user.html")
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    """Create and add a new user to the Users DB"""
+
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+    image_url = image_url if image_url else None
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
+    with app.app_context():
+        db.session.add(new_user)
+        db.session.commit()
+        db.session.refresh(new_user)
+
+    return redirect(f"/users/{new_user.id}")
