a1cb09aefeee2b4b48e3f135fa92ca4f953e1038
bc-dipesh <bcdipeshwork@gmail.com>
2023-05-22 10:28:41 -0400

    Create Post model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models for Blogly."""
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -26,3 +27,26 @@ class User(db.Model):
         user = self
 
         return f"<User id={user.id}, first_name={user.first_name}, last_name={user.last_name}, image_url={user.image_url}>"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, defaul=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    user = db.relationship("User", backref="posts")
+
+    def __repr__(self):
+        """Change the default representation of the object"""
+
+        post = self
+
+        return f"<Post id={post.id}, title={post.title}, content={post.content}, created_at={post.created_at}>"
